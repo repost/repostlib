47279f420c301635b6b4beb3a4de0f106908512f
hank <hank@bc4a479e-3a1a-4069-b84a-023495cab333>
2011-07-04 21:51:36 +0000

    Moving debug.h etc.. to safer names. More changes to get it into line. refs #43

---
+++ lib/common/rpdebug.cpp
@@ -0,0 +1,109 @@
+
+#include "rpdebug.h"
+
+const char*const LogSeverityNames[NUM_SEVERITIES] = {
+  "INFO", "WARNING", "ERROR", "FATAL"
+};
+
+LogMessage::LogMessage(const char* file, int line, LogSeverity severity) {
+  Init(file, line, severity, &LogMessage::SendToLog);
+}
+
+void LogMessage::Init(const char* file,
+                      int line,
+                      LogSeverity severity,
+                      void (LogMessage::*send_method)()) {
+
+    allocated_ = new LogMessageData();
+    data_ = allocated_;
+    data_->message_text_ = data_->buf_;
+    data_->stream_alloc_ = new LogStream(0);
+    data_->stream_ = data_->stream_alloc_;
+    data_->first_fatal_ = false;
+
+    stream().fill('0');
+    data_->preserved_errno_ = errno;
+    data_->severity_ = severity;
+    data_->line_ = line;
+    data_->send_method_ = send_method;
+    data_->sink_ = NULL;
+    data_->outvec_ = NULL;
+    WallTime now = WallTime_Now();
+    data_->timestamp_ = static_cast<time_t>(now);
+    localtime_r(&data_->timestamp_, &data_->tm_time_);
+    int usecs = static_cast<int>((now - data_->timestamp_) * 1000000);
+
+    data_->num_chars_to_log_ = 0;
+    data_->num_chars_to_syslog_ = 0;
+    data_->basename_ = const_basename(file);
+    data_->fullname_ = file;
+    data_->has_been_flushed_ = false;
+
+    // If specified, prepend a prefix to each line.  For example:
+    //    I1018 160715 f5d4fbb0 logging.cc:1153]
+    //    (log level, GMT month, date, time, thread_id, file basename, line)
+    // We exclude the thread_id for the default thread.
+    if (FLAGS_log_prefix && (line != kNoLogPrefix)) {
+        stream() << LogSeverityNames[severity][0]
+            << setw(2) << 1+data_->tm_time_.tm_mon
+            << setw(2) << data_->tm_time_.tm_mday
+            << ' '
+            << setw(2) << data_->tm_time_.tm_hour  << ':'
+            << setw(2) << data_->tm_time_.tm_min   << ':'
+            << setw(2) << data_->tm_time_.tm_sec   << "."
+            << setw(6) << usecs
+            << ' '
+            << setfill(' ') << setw(5)
+            << static_cast<unsigned int>(GetTID()) << setfill('0')
+            << ' '
+            << data_->basename_ << ':' << data_->line_ << "] ";
+    }
+    data_->num_prefix_chars_ = data_->stream_->pcount();
+}
+
+LogMessage::~LogMessage() {
+  Flush();
+  delete allocated_;
+}
+
+// Flush buffered message, called by the destructor, or any other function
+// that needs to synchronize the log.
+void LogMessage::Flush() {
+  if (data_->has_been_flushed_ || data_->severity_ < FLAGS_minloglevel)
+    return;
+
+  data_->num_chars_to_log_ = data_->stream_->pcount();
+  data_->num_chars_to_syslog_ =
+    data_->num_chars_to_log_ - data_->num_prefix_chars_;
+
+  // Do we need to add a \n to the end of this message?
+  bool append_newline =
+      (data_->message_text_[data_->num_chars_to_log_-1] != '\n');
+  char original_final_char = '\0';
+
+  // If we do need to add a \n, we'll do it by violating the memory of the
+  // ostrstream buffer.  This is quick, and we'll make sure to undo our
+  // modification before anything else is done with the ostrstream.  It
+  // would be preferable not to do things this way, but it seems to be
+  // the best way to deal with this.
+  if (append_newline) {
+    original_final_char = data_->message_text_[data_->num_chars_to_log_];
+    data_->message_text_[data_->num_chars_to_log_++] = '\n';
+  }
+
+  // Prevent any subtle race conditions by wrapping a mutex lock around
+  // the actual logging action per se.
+  {
+    (this->*(data_->send_method_))();
+  }
+
+  if (append_newline) {
+    // Fix the ostrstream back how it was before we screwed with it.
+    // It's 99.44% certain that we don't need to worry about doing this.
+    data_->message_text_[data_->num_chars_to_log_-1] = original_final_char;
+  }
+
+  // Note that this message is now safely logged.  If we're asked to flush
+  // again, as a result of destruction, say, we'll do nothing on future calls.
+  data_->has_been_flushed_ = true;
+}
---
+++ lib/include/rpdebug.h
@@ -0,0 +1,178 @@
+#ifndef RPDEBUG_H_
+#define RPDEBUG_H_
+
+/**
+ * Alot of inspiration taken from glog
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+#if REPOST_STRIP_LOG == 0
+#define COMPACT_REPOST_LOG_INFO repost::LogMessage( \
+      __FILE__, __LINE__)
+#define LOG_TO_STRING_INFO(message) repost::LogMessage( \
+      __FILE__, __LINE__, repost::INFO, message)
+#else
+#define COMPACT_REPOST_LOG_INFO repost::NullStream()
+#define LOG_TO_STRING_INFO(message) repost::NullStream()
+#endif
+
+#if REPOST_STRIP_LOG <= 1
+#define COMPACT_REPOST_LOG_WARNING repost::LogMessage( \
+      __FILE__, __LINE__, repost::WARNING)
+#define LOG_TO_STRING_WARNING(message) repost::LogMessage( \
+      __FILE__, __LINE__, repost::WARNING, message)
+#else
+#define COMPACT_REPOST_LOG_WARNING repost::NullStream()
+#define LOG_TO_STRING_WARNING(message) repost::NullStream()
+#endif
+
+#if REPOST_STRIP_LOG <= 2
+#define COMPACT_REPOST_LOG_ERROR repost::LogMessage( \
+      __FILE__, __LINE__, repost::ERROR)
+#define LOG_TO_STRING_ERROR(message) repost::LogMessage( \
+      __FILE__, __LINE__, repost::ERROR, message)
+#else
+#define COMPACT_REPOST_LOG_ERROR repost::NullStream()
+#define LOG_TO_STRING_ERROR(message) repost::NullStream()
+#endif
+
+#if REPOST_STRIP_LOG <= 3
+#define COMPACT_REPOST_LOG_FATAL repost::LogMessageFatal( \
+      __FILE__, __LINE__)
+#define LOG_TO_STRING_FATAL(message) repost::LogMessage( \
+      __FILE__, __LINE__, repost::FATAL, message)
+#else
+#define COMPACT_REPOST_LOG_FATAL repost::NullStreamFatal()
+#define LOG_TO_STRING_FATAL(message) repost::NullStreamFatal()
+#endif
+
+#define LOG_IF(severity, condition) \
+  !(condition) ? (void) 0 : repost::LogMessageVoidify() & LOG(severity)
+
+#define LOG(severity) COMPACT_REPOST_LOG_ ## severity.stream()
+
+typedef int LogSeverity;
+
+const int INFO = 0, WARNING = 1, ERROR = 2, FATAL = 3, NUM_SEVERITIES = 4;
+
+// DFATAL is FATAL in debug mode, ERROR in normal mode
+#ifdef NDEBUG
+#define DFATAL_LEVEL ERROR
+#else
+#define DFATAL_LEVEL FATAL
+#endif
+
+extern const char* const LogSeverityNames[NUM_SEVERITIES];
+
+// This class is used to explicitly ignore values in the conditional
+// logging macros.  This avoids compiler warnings like "value computed
+// is not used" and "statement has no effect".
+
+class LogMessageVoidify {
+ public:
+  LogMessageVoidify() { }
+  // This has to be an operator with a precedence lower than << but
+  // higher than ?:
+  void operator&(std::ostringstream&) { }
+};
+
+class LogMessage {
+public:
+  /* LogStream inherit from non-DLL-exported class (std::ostrstream)
+  ** and VC++ produces a warning for this situation.
+  ** However, MSDN says "C4275 can be ignored in Microsoft Visual C++
+  ** 2005 if you are deriving from a type in the Standard C++ Library"
+  ** http://msdn.microsoft.com/en-us/library/3tdb471s(VS.80).aspx
+  ** Let's just ignore the warning.
+  */
+#ifdef _MSC_VER
+# pragma warning(disable: 4275)
+#endif
+  class LogStream : public std::ostringstream {
+#ifdef _MSC_VER
+# pragma warning(default: 4275)
+#endif
+  public:
+    LogStream(int ctr)
+      : ostringstream(std::ostringstream::out),
+        ctr_(ctr) {
+      self_ = this;
+    }
+
+    int ctr() const { return ctr_; }
+    void set_ctr(int ctr) { ctr_ = ctr; }
+    LogStream* self() const { return self_; }
+
+  private:
+    int ctr_;  // Counter hack (for the LOG_EVERY_X() macro)
+    LogStream *self_;  // Consistency check hack
+  };
+
+public:
+  // icc 8 requires this typedef to avoid an internal compiler error.
+  typedef void (LogMessage::*SendMethod)();
+
+  LogMessage(const char* file, int line, LogSeverity severity);
+  ~LogMessage();
+  
+  // Flush a buffered message to the sink set in the constructor.  Always
+  // called by the destructor, it may also be called from elsewhere if
+  // needed.  Only the first call is actioned; any later ones are ignored.
+  void Flush();
+
+
+  std::ostringstream& stream() { return *(data_->stream_); }
+
+private:
+  void Init(const char* file, int line, LogSeverity severity,
+            void (LogMessage::*send_method)());
+
+  // We keep the data in a separate struct so that each instance of
+  // LogMessage uses less stack space.
+  struct LogMessageData {
+    LogMessageData() {};
+
+    int preserved_errno_;      // preserved errno
+    char* message_text_;  // Complete message text (points to selected buffer)
+    LogStream* stream_alloc_;
+    LogStream* stream_;
+    char severity_;      // What level is this LogMessage logged at?
+    int line_;                 // line number where logging call is.
+    void (LogMessage::*send_method_)();  // Call this in destructor to send
+    union {  // At most one of these is used: union to keep the size low.
+      std::string* message_;             // NULL or string to write message into
+    };
+    time_t timestamp_;            // Time of creation of LogMessage
+    struct ::tm tm_time_;         // Time of creation of LogMessage
+    size_t num_prefix_chars_;     // # of chars of prefix in this message
+    size_t num_chars_to_log_;     // # of chars of msg to send to log
+    size_t num_chars_to_syslog_;  // # of chars of msg to send to syslog
+    const char* basename_;        // basename of file that called LOG
+    const char* fullname_;        // fullname of file that called LOG
+    bool has_been_flushed_;       // false => data has not been flushed
+    bool first_fatal_;            // true => this was first fatal msg
+
+    ~LogMessageData();
+   private:
+    LogMessageData(const LogMessageData&);
+    void operator=(const LogMessageData&);
+  };
+
+  static LogMessageData fatal_msg_data_exclusive_;
+  static LogMessageData fatal_msg_data_shared_;
+
+  LogMessageData* allocated_;
+  LogMessageData* data_;
+
+  LogMessage(const LogMessage&);
+  void operator=(const LogMessage&);
+};
+
+#endif
